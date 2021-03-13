e2b00562d517201eb1f8aadce286e5b18986d518
Hectarea <hectometrocuadrado@gmail.com>
2021-03-13 13:17:05 +0100

    Added use of cvector a cvar in physical-device.rkt

--- src/physical-device.rkt
+++ src/physical-device.rkt
@@ -1,9 +1,11 @@
 #lang racket/base
 
 
-(require "util/util.rkt")
-(require vulkan/unsafe)
-(require ffi/unsafe)
+(require "cvar.rkt"
+         "array.rkt"
+         vulkan/unsafe
+         ffi/cvector
+         ffi/unsafe)
 
 (provide get-physical-device
          get-family-queues-properties
@@ -18,17 +20,14 @@
 ; Retorna una lista con todos los dispositivos
 (define (enumerate-physical-devices instance)
 
-  (define physical-device-count-ptr (malloc _uint32))
-  (define count-result (vkEnumeratePhysicalDevices instance physical-device-count-ptr #f))
-  (when (not (equal? count-result VK_SUCCESS))
-    (error 'enumerate-physical-devices "Error al enumerar los dispositivos."))
-  (define physical-device-count (ptr-ref physical-device-count-ptr _uint32))
+  (define physical-device-count (make-cvar _uint32))
+  (define count-result (vkEnumeratePhysicalDevices instance (cvar-ptr physical-device-count) #f))
+  (check-vkResult count-result 'enumerate-physical-devices)
   (when (not (> physical-device-count 0))
-    (error 'enumerate-physical-devices "El numero de dispositivos no es positivo."))
-  (define physical-devices-ptr (malloc _VkPhysicalDevice physical-device-count))
-  (vkEnumeratePhysicalDevices instance physical-device-count-ptr physical-devices-ptr)
-  (for/list ([i (build-list physical-device-count values)])
-    (ptr-ref physical-devices-ptr _VkPhysicalDevice)))
+    (error 'enumerate-physical-devices "No physical devices found"))
+  (define physical-devices (make-cvector _VkPhysicalDevice (cvar-ref (cvar-ref physical-device-count))))
+  (vkEnumeratePhysicalDevices instance (cvar-ref physical-device-count) (cvector-ptr physical-devices))
+  (cvector->list physical-devices))
 
 
 ; --- device type ---
@@ -37,10 +36,10 @@
 ; Comprueba que un dispositivo es de cierto tipo
 (define (check-type physical-device type)
 
-  (define properties-ptr (cast (malloc _VkPhysicalDeviceProperties) _pointer _VkPhysicalDeviceProperties-pointer))
-  (vkGetPhysicalDeviceProperties physical-device properties-ptr)
+  (define properties (make-cvar _VkPhysicalDeviceProperties))
+  (vkGetPhysicalDeviceProperties physical-device (cvar-ptr properties))
 
-  (equal? (VkPhysicalDeviceProperties-deviceType properties-ptr) type))
+  (equal? (VkPhysicalDeviceProperties-deviceType (cvar-ref properties)) type))
 
 
 ; --- device extensions ---
@@ -49,14 +48,12 @@
 ; Devuelve las extensiones disponibles en el dispositivo
 (define (get-available-extensions physical-device)
 
-  (define extension-count-ptr (malloc _uint32))
-  (vkEnumerateDeviceExtensionProperties physical-device #f extension-count-ptr #f)
-  (define extension-count (ptr-ref extension-count-ptr _uint32))
-  (define available-extensions-ptr (malloc _VkExtensionProperties extension-count))
-  (vkEnumerateDeviceExtensionProperties physical-device #f extension-count-ptr available-extensions-ptr)
+  (define extension-count (make-cvar _uint32))
+  (vkEnumerateDeviceExtensionProperties physical-device #f (cvar-ptr extension-count) #f)
+  (define available-extensions (make-cvector _VkExtensionProperties (cvar-ref extension-count)))
+  (vkEnumerateDeviceExtensionProperties physical-device #f (cvar-ptr extension-count) (cvector-ptr available-extensions))
   
-  (for/list ([i (build-list extension-count values)])
-    (ptr-ref available-extensions-ptr _VkExtensionProperties i)))
+  (cvector->list available-extensions))
 
 
 ; Comprueba la disponibilidad de las extensiones en el dispositivo
@@ -66,7 +63,7 @@
 
   (for/and ([required-extension required-extensions])
     (for/or ([available-extension available-extensions])
-      (zero? (strcmp required-extension (array-ptr (VkExtensionProperties-extensionName available-extension)))))))
+      (equal? required-extension (array->bytes (VkExtensionProperties-extensionName available-extension))))))
 
 
 ; --- family queues ---
@@ -75,14 +72,12 @@
 ; Devuelve las propiedades de las familias de colas de un dispositivo
 (define (get-family-queues-properties physical-device)
 
-  (define family-queues-count-ptr (malloc _uint32))
-  (vkGetPhysicalDeviceQueueFamilyProperties physical-device family-queues-count-ptr #f)
-  (define family-queues-count (ptr-ref family-queues-count-ptr _uint32))
-  (define family-queues-properties-ptr (malloc _VkQueueFamilyProperties family-queues-count))
-  (vkGetPhysicalDeviceQueueFamilyProperties physical-device family-queues-count-ptr family-queues-properties-ptr)
+  (define family-queues-count (make-cvar _uint32))
+  (vkGetPhysicalDeviceQueueFamilyProperties physical-device (cvar-ptr family-queues-count) #f)
+  (define family-queues-properties (make-cvector _VkQueueFamilyProperties (cvar-ref family-queues-count)))
+  (vkGetPhysicalDeviceQueueFamilyProperties physical-device (cvar-ptr family-queues-count) (cvector-ptr family-queues-properties))
   
-  (for/list ([i (build-list family-queues-count values)])
-    (ptr-ref family-queues-properties-ptr _VkQueueFamilyProperties i)))
+  (cvector->list family-queues-properties))
 
 
 ; Comprueba la validez de las familias de colas de un dispositivo
@@ -91,11 +86,11 @@
   (define family-queues-properties (get-family-queues-properties physical-device))
 
   (for/fold ([saved-flags 0] [saved-present-queue #f] [result #f] #:result result)
-            ([i (build-list (length family-queues-properties) values)] [family-property family-queues-properties] #:break (equal? result #t))
+            ([i (build-list (length family-queues-properties) values)] [family-property family-queues-properties] #:break result)
     (define current-flags (bitwise-and (VkQueueFamilyProperties-queueFlags family-property) required-flags))
-    (define present-queue-ptr (malloc _VkBool32))
-    (vkGetPhysicalDeviceSurfaceSupportKHR physical-device i surface present-queue-ptr)
-    (define current-present-queue (or saved-present-queue (ptr-ref present-queue-ptr _VkBool32)))
+    (define present-queue (make-cvar _VkBool32))
+    (vkGetPhysicalDeviceSurfaceSupportKHR physical-device i surface (cvar-ptr present-queue))
+    (define current-present-queue (or saved-present-queue (cvar-ref present-queue)))
     (values current-flags current-present-queue (and (equal? current-flags required-flags) (equal? current-present-queue VK_TRUE)))))
 
 
@@ -105,10 +100,10 @@
 ; Comprueba la disponibilidad de las caracteristicas del dispositivo
 (define (check-features physical-device checker)
 
-  (define features-ptr (cast (malloc _VkPhysicalDeviceFeatures) _pointer _VkPhysicalDeviceFeatures-pointer))
-  (vkGetPhysicalDeviceFeatures physical-device features-ptr)
+  (define features (make-cvar _VkPhysicalDeviceFeatures))
+  (vkGetPhysicalDeviceFeatures physical-device (cvar-ptr features))
 
-  (checker features-ptr))
+  (checker (cvar-ptr features)))
 
 
 ; --- surface presentation ---
@@ -117,38 +112,34 @@
 ; Devuelve las prestaciones de una surface
 (define (get-surface-capabilities physical-device surface)
 
-  (define capabilities-ptr (malloc _VkSurfaceCapabilitiesKHR))
-  (vkGetPhysicalDeviceSurfaceCapabilitiesKHR physical-device surface capabilities-ptr)
+  (define capabilities (make-cvar _VkSurfaceCapabilitiesKHR))
+  (vkGetPhysicalDeviceSurfaceCapabilitiesKHR physical-device surface (cvar-ptr capabilities))
 
-  (cast capabilities-ptr _pointer _VkSurfaceCapabilitiesKHR-pointer))
+  (cvar-ptr capabilities))
 
 
 
 ; Devuelve los formatos de surface disponibles en el dispositivo
 (define (get-surface-formats physical-device surface)
 
-  (define format-count-ptr (malloc _uint32))
-  (vkGetPhysicalDeviceSurfaceFormatsKHR physical-device surface format-count-ptr #f)
-  (define format-count (ptr-ref format-count-ptr _uint32))
-  (define formats-ptr (malloc _VkSurfaceFormatKHR format-count))
-  (vkGetPhysicalDeviceSurfaceFormatsKHR physical-device surface format-count-ptr formats-ptr)
+  (define format-count (make-cvar _uint32))
+  (vkGetPhysicalDeviceSurfaceFormatsKHR physical-device surface (cvar-ptr format-count) #f)
+  (define formats (make-cvector _VkSurfaceFormatKHR format-count))
+  (vkGetPhysicalDeviceSurfaceFormatsKHR physical-device surface (cvar-ptr format-count) (cvector-ptr formats))
 
-  (for/list ([i (build-list format-count values)])
-    (ptr-ref formats-ptr _VkSurfaceFormatKHR i)))
+  (cvector->list formats))
 
 
 
 ; Devuelve los modos de presentacion de un surface en el dispositivo
 (define (get-surface-present-modes physical-device surface)
 
-  (define mode-count-ptr (malloc _uint32))
-  (vkGetPhysicalDeviceSurfacePresentModesKHR physical-device surface mode-count-ptr #f)
-  (define mode-count (ptr-ref mode-count-ptr _uint32))
-  (define present-modes-ptr (malloc _VkPresentModeKHR mode-count))
-  (vkGetPhysicalDeviceSurfacePresentModesKHR physical-device surface mode-count-ptr present-modes-ptr)
+  (define mode-count (make-cvar _uint32))
+  (vkGetPhysicalDeviceSurfacePresentModesKHR physical-device surface (cvar-ptr mode-count) #f)
+  (define present-modes (make-cvector _VkPresentModeKHR mode-count))
+  (vkGetPhysicalDeviceSurfacePresentModesKHR physical-device surface (cvar-ptr mode-count) (cvector-ptr present-modes))
 
-  (for/list ([i (build-list mode-count values)])
-    (ptr-ref present-modes-ptr _VkPresentModeKHR i)))
+  (cvector->list present-modes))
 
 
 ; Comprueba la validez de un surface
@@ -178,9 +169,9 @@
           physical-device
           #f)))
 
-  (if (not the-physical-device)
-      (error 'get-physical-device "No se ha encontrado un dispositivo valido.")
-      the-physical-device))
+  (when (not the-physical-device)
+    (error 'get-physical-device "No valid physical device found"))
+  the-physical-device)
 
 
 ; --- memory type ---
@@ -189,21 +180,21 @@
 ; Devuelve el tipo de memoria que cumple unos requisitos
 (define (get-memory-type physical-device type-filter property-flags)
 
-  (define mem-properties-ptr (malloc _VkPhysicalDeviceMemoryProperties))
-  (vkGetPhysicalDeviceMemoryProperties physical-device mem-properties-ptr)
+  (define mem-properties (make-cvar _VkPhysicalDeviceMemoryProperties))
+  (vkGetPhysicalDeviceMemoryProperties physical-device (cvar-ptr mem-properties))
 
   (define mem-type
-    (for/or ([i (build-list (VkPhysicalDeviceMemoryProperties-memoryTypeCount mem-properties-ptr) values)])
+    (for/or ([i (build-list (VkPhysicalDeviceMemoryProperties-memoryTypeCount (cvar-ref mem-properties)) values)])
       (if (and (not (zero? (bitwise-and type-filter (arithmetic-shift 1 i))))
                (bitwise-and (VkMemoryType-propertyFlags
-                             (ptr-add (array-ptr (VkPhysicalDeviceMemoryProperties-memoryTypes mem-properties-ptr)) i _VkMemoryType))
-                            property-flags))
+                             (array-ref (VkPhysicalDeviceMemoryProperties-memoryTypes (cvar-ref mem-properties)) i)
+                             property-flags)))
           i
           #f)))
 
-  (if (not mem-type)
-      (error 'get-memory-type "No existe un tipo de memoria con los requisitos necesarios.")
-      mem-type))
+  (when (not mem-type)
+    (error 'get-memory-type "No existe un tipo de memoria con los requisitos necesarios."))
+  mem-type)
 
 
 ; --- format ---
@@ -214,18 +205,18 @@
 
   (define the-format
     (for/or ([format possible-formats])
-      (define format-properties-ptr (malloc _VkFormatProperties))
-      (vkGetPhysicalDeviceFormatProperties physical-device format format-properties-ptr)
+      (define format-properties (make-cvar _VkFormatProperties))
+      (vkGetPhysicalDeviceFormatProperties physical-device format (cvar-ptr format-properties))
 
       (cond
         [(and (equal? tiling VK_IMAGE_TILING_LINEAR)
-              (equal? (bitwise-and (VkFormatProperties-linearTilingFeatures format-properties-ptr) features) features))
+              (equal? (bitwise-and (VkFormatProperties-linearTilingFeatures (cvar-ref format-properties)) features) features))
          format]
         [(and (equal? tiling VK_IMAGE_TILING_OPTIMAL)
-              (equal? (bitwise-and (VkFormatProperties-optimalTilingFeatures format-properties-ptr) features) features))
+              (equal? (bitwise-and (VkFormatProperties-optimalTilingFeatures (cvar-ref format-properties)) features) features))
          format]
         [else #f])))
 
-  (if (not the-format)
-      (error 'get-format "No se encuentra ningun formato con los requisitos necesarios.")
-      the-format))
+  (when (not the-format)
+    (error 'get-format "No se encuentra ningun formato con los requisitos necesarios."))
+  the-format)
